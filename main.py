05c7c2205e0eed582e03c4809312c980d573e7c1
frozenfirebomb <bartvrugt@hotmail.com>
2025-12-04 21:57:38 -0500

    main function now properly checks if something is is valid for chapter 98. Now working on metal declaration.

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ from openpyxl.styles import Border, Side
 import xlrd
 from xlrd import open_workbook, cellname
 import xlwt
+from xlwt import Workbook
 import xlutils
 from xlutils.copy import copy
 
-from file_paths import invnday_path, po_us_vender_path, invnday_save_path #, metal_master_path, steel_codes_path, alum_codes_path, copper_codes_path, save_path, report_path
+from file_paths import invnday_path, po_us_vender_path, steel_codes_path, alum_codes_path, copper_codes_path, invnday_save_path #, metal_master_path, save_path, report_path
 from chapter_98 import chapter_98_codes
 
 today = date.today().isoformat() #.replace("-","")
@@ -33,6 +34,24 @@ final_invnday_ws = final_invnday_wb.get_sheet(0)
 
 po_us_vendor_wb = openpyxl.load_workbook(latest_us_po_vendor)
 po_us_vendor_ws = po_us_vendor_wb['Sheet1']
+
+def harm_codes(file_path):  # Returns the contents of a text file as a list of strings from a file path.
+    harm_codes = []
+    with open(file_path) as f:
+        file_contents = f.read()    
+    for content in file_contents.split():
+        harm_codes.append(content)
+    return harm_codes 
+
+def metal_declaration_check(row_index):
+    for harm_code in harm_codes(alum_codes_path):
+        value = invnday_sheet.cell(row_index, 10).value
+        if value[:len(harm_code)] == harm_code:
+            print(value, harm_code )
+            print("declaration required")
+            break
+
+metal_declaration_check(42)
 
 def acquire_chapter_98(harm_code):
     for key, value in chapter_98_codes.items():
@@ -58,7 +77,8 @@ def actual_po_date(po_row):
         return False
 
 def main():
-    for row_index in range(invnday_sheet.nrows):
+    tracker = 0
+    for row_index in range(1, invnday_sheet.nrows):
         invnday_sku = invnday_sheet.cell(row_index, 3).value
         start_of_chapter_98 = "9801"
         for po_row in range(1, po_us_vendor_ws.max_row + 1):
@@ -68,22 +88,33 @@ def main():
                 if valid_for_chapter_98 == True:
                     
                     if invnday_sheet.cell(row_index, 10).value[:4] == start_of_chapter_98:
-                        print("correct sku")
+                        print("correct sku", row_index + 1)
+                        tracker += 1
                     else:
-                        print("changed to", acquire_chapter_98(invnday_sheet.cell(row_index, 10).value))
+                        final_invnday_ws.write(row_index, 10, acquire_chapter_98(invnday_sheet.cell(row_index, 10).value))
+                        print("changed to", acquire_chapter_98(invnday_sheet.cell(row_index, 10).value), row_index + 1)
+                        tracker += 1
                 else:
                     if invnday_sheet.cell(row_index, 10).value[:4] == start_of_chapter_98:
-                        print("not valid on row: ", row_index + 1)
+                        print(f"Sku ({invnday_sheet.cell(row_index, 3).value}) has a PO purchase date that exceeds 3 years. Please change from chapter 98.", row_index + 1)
+                        tracker += 1
+                    else:
+                        print("keep the same", row_index + 1)  # declaration function should go here.
+                        tracker += 1
                 break
             
         else:
             if invnday_sheet.cell(row_index, 10).value[:4] == start_of_chapter_98:
-                print("not valid on row: ", row_index + 1)
+                print(f"Sku ({invnday_sheet.cell(row_index, 3).value}) has a PO purchase date that exceeds 3 years. Please change from chapter 98.", row_index + 1)
+                tracker += 1
             
             else:
-                print("keep the same")
+                print("keep the same", row_index + 1)  # declaration function should go here.
+                tracker += 1
+    print("tracker: ", tracker)
 
 # main()
-print("test: ", latest_invnday)
-final_invnday_save = latest_invnday.split("/")[-1]
-final_invnday_wb.save(f"{invnday_save_path}/final_{final_invnday_save}")
+
+
+# final_invnday_save = latest_invnday.split("/")[-1]
+# final_invnday_wb.save(f"{invnday_save_path}/final_{final_invnday_save}")
